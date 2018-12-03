83a86c929065fd8dba150bc48fd8dd18abd6d616
kluhpas <andrea.rota.98@gmail.com>
2018-12-03 13:38:59 +0100

    1.0
    Prima versione.
    Assenza file "About Me".

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="it-IT">
+<head>
+  <title>Andrea Rota</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="apple-touch-icon" sizes="57x57" href="favicon/apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="57x57" href="favicon/apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="favicon/apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="favicon/apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="favicon/apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="favicon/apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="favicon/apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="favicon/apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="favicon/apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192"  href="favicon/android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="favicon/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+  <link rel="manifest" href="favicon/manifest.json">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="msapplication-TileImage" content="favicon/ms-icon-144x144.png">
+  <meta name="theme-color" content="#ffffff">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="index.css">
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous"></head>
+<body>
+  <nav class="navbar sticky-top navbar-expand-sm navbar-dark">
+
+      <a class="navbar-brand mb-0 display-4" href="index.php">Andrea Rota</a>
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarToggler">
+      <ul class="navbar-nav ml-auto text-center">
+        <li class="nav-item">
+          <a class="nav-link" href="#">About Me</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://www.instagram.com/andrearoota/" target="_blank"><i class="fab fa-instagram"></i></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://www.facebook.com/andrea.rota.520" target="_blank"><i class="fab fa-facebook"></i></a>
+        </li>
+      </ul>
+    </div>
+
+  </nav>
+  <div class="album py-4">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-4">
+          <a href="https://play.google.com/store/apps/details?id=com.kluhpas.kummerproject" target="_blank">
+            <div class="card bg-dark text-white border-white">
+              <img class="card-img" src="media/img_rapility_android.jpg" alt="Rapility for Android">
+              <div class="card-img-overlay d-flex flex-column justify-content-end">
+                <h5 class="card-title">Rapility for Android</h5>
+              </div> <!-- .card-img-overlay -->
+            </div> <!-- .card -->
+          </a>
+        </div> <!-- .col-sm-4 -->
+        <div class="col-sm-4">
+          <a href="#">
+            <div class="card bg-dark text-white border-white work-in-progress">
+              <img class="card-img" src="media/img_rapility_ios.jpg" alt="Rapility for iOS">
+              <div class="card-img-overlay d-flex flex-column justify-content-end work-in-progress">
+                <h5 class="card-title work-in-progress">Rapility for iOS</h5>
+              </div> <!-- .card-img-overlay -->
+            </div> <!-- .card -->
+          </a>
+        </div> <!-- .col-sm-4 -->
+        <div class="col-sm-4">
+          <a href="rapilityWeb/index.html">
+            <div class="card bg-dark text-white border-white">
+              <img class="card-img" src="media/img_rapility_web.jpg" alt="Rapility for Web">
+              <div class="card-img-overlay d-flex flex-column justify-content-end">
+                <h5 class="card-title">Rapility for Web</h5>
+              </div> <!-- .card-img-overlay -->
+            </div> <!-- .card -->
+          </a>
+        </div> <!-- .col-sm-4 -->
+      </div> <!-- .row -->
+    </div> <!-- .container -->
+  </div> <!-- .album -->
+</body>
+</html>
